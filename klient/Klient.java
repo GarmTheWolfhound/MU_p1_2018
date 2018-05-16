e8c4cbb2bb9df40d7b0d35b5ab5af532f5a6c9a9
Daniel Lone <37078735+Zeeshan1992@users.noreply.github.com>
2018-05-16 14:31:54 +0200

    Add files via upload

---
+++ klient/Klient.java
@@ -0,0 +1,40 @@
+package klient;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+import javax.imageio.ImageIO;
+
+public class Klient {
+	private String ip;
+	private int port;
+	
+	public Klient(String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+	
+	public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1", 13085);
+        OutputStream outputStream = socket.getOutputStream();
+
+        BufferedImage image = ImageIO.read(new File("images/mario.jpg"));
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", byteArrayOutputStream);
+
+        byte[] size = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array();
+        outputStream.write(size);
+        outputStream.write(byteArrayOutputStream.toByteArray());
+        outputStream.flush();
+        System.out.println("Flushed: " + System.currentTimeMillis());
+
+        Thread.sleep(120000);
+        System.out.println("Closing: " + System.currentTimeMillis());
+        socket.close();
+    }
+}
