08c80bf198393c8bf6e4abecb59701b463956927
GarmTheWolfhound <34164154+GarmTheWolfhound@users.noreply.github.com>
2018-05-16 15:42:37 +0200

    Add files via upload

---
+++ P1_connectToServer.ino
@@ -0,0 +1,61 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+//wi-fi info
+const char* ssid     = "AndroidAP";
+const char* password = "exqc4047";
+byte mac[6];
+
+//Server info
+IPAddress server_IP(192,168,43,117);
+WiFiClient client;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+  Serial.println("");
+  
+  if(client.connect(server_IP, port)){
+    Serial.println("Connected to server");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
