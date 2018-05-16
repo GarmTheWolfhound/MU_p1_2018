fd4978031ff5d411051c84edd76c91a0c9493e27
Daniel Lone <37078735+Zeeshan1992@users.noreply.github.com>
2018-05-16 18:28:15 +0200

    Add files via upload

---
+++ connectionTest/conn.ino
@@ -0,0 +1,53 @@
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+ 
+const char* ssid     = "HUAWEI";
+const char* password = "shani111";
+IPAddress server(192, 168, 43, 58);
+int port = 12345;
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+ 
+  // We start by connecting to a WiFi network
+ 
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Now we try to connect to the Server
+  
+  Serial.print("Attempting conenction to server... ");
+  if(client.connect(server, port)){
+        Serial.println("SUCSESS");
+    }
+    else{
+        Serial.println("FAILED");
+    }
+}
+
+void loop() {
+    if (client.available()) {
+    char c = client.read();
+    Serial.println(c);
+  }else if(!client.connected()){
+    //Serial.println("disconnecting");
+    client.stop();
+  }
+
+}
