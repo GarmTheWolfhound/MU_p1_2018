b73e94209476ac3dd5be00e0c7f2b9cc9317b35d
GarmTheWolfhound <34164154+GarmTheWolfhound@users.noreply.github.com>
2018-05-29 17:00:55 +0200

    Update p1_IS.ino

--- _Embedded/p1_IS.ino
+++ _Embedded/p1_IS.ino
@@ -1,3 +1,5 @@
+//Written by Oskar Adolfsson
+
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 
@@ -8,13 +10,13 @@
 #define INTERRUPT_PIN 2
 
 //sets constants for the Wi-Fi to be used.
-const char* ssid     = "AndroidAP";
-const char* password = "exqc4047";
+const char* ssid     = "SSID"; //The name of the network
+const char* password = "password"; //The password for the network
 
 WiFiClient client;
-IPAddress server(192, 168, 43, 117); //IP of the server
+IPAddress server(192, 168, 43, 58); //IP of the server
 
-int port = 12345;
+int port = 33333;
 int previousControlByte = -1; //Will be used to check if a new array is availible
 
 //The array that has the values for the LEDs. Changes based on server content.
@@ -139,7 +141,7 @@ int imageArray[120][16] = {
                             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-                          } 
+                          };
                           
 int j, i; //Controlls writing to LEDs.
 
@@ -151,9 +153,12 @@ float avrageLap;
 int avrageArrayPos = 0;
 float avrageLapTime[10] ={0, 0, 0, 0, 0, 0, 0, 0, 0, 0};//the 10 latest values are stored
 long microDelay = 0;
+long serverConnectBegin;
 
 
 void setup() {
+  //wdt_disable();
+  //wdt_enable(65535);
   Serial.begin(115200);
   delay(100);
  
@@ -171,14 +176,15 @@ void setup() {
     Serial.print(".");
   }
  
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+ // Serial.println("");
+ // Serial.println("WiFi connected");  
+ // Serial.println("IP address: ");
+ // Serial.println(WiFi.localIP());
 
 
-  // tries to connect to the server
-  Serial.print("Attempting conenction to server... ");
+  //tries to connect to the server
+  Serial.print("Attempting connection to server... ");
+  serverConnectBegin = micros();
   if(client.connect(server, port)){
         Serial.println("SUCSESS");
   }else{
@@ -190,25 +196,34 @@ void setup() {
   pinMode(SHIFT_CLK_PIN, OUTPUT);
   pinMode(STORE_CLK_PIN,OUTPUT);
   pinMode(INTERRUPT_PIN, INPUT);
-
+  digitalWrite(INTERRUPT_PIN, HIGH);
+  
+  Serial.print("Waiting 10 seconds... ");
   delay(10000);//Allows the motor to start
-  
-  while(INTERRUPT_PIN != LOW){}//Do nothing until the sensor is triggered...
-  
+  Serial.println("Done");
+ 
+  while(INTERRUPT_PIN != 0){}//Do nothing until the sensor is triggered...
   previousMicros = micros();//...and get the time
   while(avrageArrayPos < 10){
-    if(INTERRUPT_PIN == LOW){
+    int val = digitalRead(INTERRUPT_PIN);
+    if(val == 0){
       currentMicros = micros();//get the time whenever the sensor is triggered
       lapTime = currentMicros - previousMicros;//get the time between laps
       previousMicros = currentMicros; 
       avrageLapTime[avrageArrayPos++] = lapTime;
+      Serial.println(avrageArrayPos);
     }
   }
+  Serial.println("Have the values");
   avrageArrayPos = avrageArrayPos % 10; //makes sure value remains between 0 and 9
   avrageLap = getAvrage();
   microDelay = avrageLap / 120; //determines the delay in micros
+  Serial.print("Delay is ");
+  Serial.print(microDelay);
+  Serial.println(" microseconds");
   
   attachInterrupt(INTERRUPT_PIN, ISR, FALLING); //enables an interrupt on the sensor
+//  ESP.wdtEnable();
 }
 
 //returns the avrage value of the array
@@ -240,6 +255,7 @@ void readFromServer() {
     //if thre's no connection, try to connect again
     if(!client.connected()){
       Serial.print("Attempting conenction to server... ");
+      serverConnectBegin = micros();
        if(client.connect(server, port)){
         Serial.println("SUCSESS");
        }else{
@@ -250,7 +266,7 @@ void readFromServer() {
     Serial.println("Attampting to read from server...");
     if (client.connected()) {
     int serverByte = client.read(); 
-    if((serverByte == 3){//2 and 3 used as to not cause an error if the programm reads a 1 when it should not. 3 means that the array havn't been read.
+    if(serverByte == 3){//2 and 3 used as to not cause an error if the programm reads a 1 when it should not. 3 means that the array havn't been read.
       Serial.println(serverByte);
       for(int loop_i = 0; loop_i < 120; loop_i++){//Copies the bytes from the server to the image array
         Serial.print("{");
@@ -264,12 +280,12 @@ void readFromServer() {
       }
     }
   }
-  
+  /*
   if(!client.connected()){//Makes sure he client always disconnects
     //Serial.println("disconnecting");
     client.stop();
   }
-  
+  */
 }
 
 //Forces array out of bounds and determines a new delay time
@@ -282,12 +298,17 @@ void ISR(){
   avrageLapTime[avrageArrayPos] = lapTime;
   avrageArrayPos = ++avrageArrayPos % 10;
   microDelay = getAvrage() / 120;
+  Serial.print("Delay: ");
+  Serial.println(microDelay);
+  
+  delay(2);//Deals with bounce
 }
 
 void loop(){
-  readFromServer();
-  Serial.println("Read attempted");
+  long timeStamp = micros();
+  if(timeStamp - serverConnectBegin >= 10000000){//Makes sure connection is only attempted every 10 seconds
+    readFromServer();
+    Serial.println("Read attempted");
+  }
   writeLED();
-  //delay(3000);
 }
-
